a9a3dfca25cb1e455adee62b2d82e3fc316b6045
Aleksandar Joksimovic <aleksandarjoksimovic996@gmail.com>
2021-05-31 14:53:26 +0200

    Initial commit

---
+++ Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/TennisSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.levi9.internship.TennisScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisSchedulerApplication.class, args);
+	}
+
+}
---
+++ Tennis-Scheduler/src/test/java/com/levi9/internship/TennisScheduler/TennisSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi9.internship.TennisScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
