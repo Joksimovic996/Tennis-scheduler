68004a6243bbcbe3ae8a68f1606a8230bc47abb5
Aleksandar Joksimovic <aleksandarjoksimovic996@gmail.com>
2021-06-01 15:02:20 +0200

    Swagger-configuration

--- Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/controller/TennisCourtController.java
+++ Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/controller/TennisCourtController.java
@@ -1,9 +1,29 @@
 package com.levi9.internship.TennisScheduler.controller;
 
+import com.levi9.internship.TennisScheduler.serviceImpl.TennisCourtServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tennis-court")
+@RequestMapping("/tennis/tennis-court")
 public class TennisCourtController {
+
+    private final TennisCourtServiceImpl courtService;
+
+    public TennisCourtController(TennisCourtServiceImpl courtService) {
+        this.courtService = courtService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTennisCourt(@PathVariable Long id) {
+        return ResponseEntity.ok(courtService.getTennisCourt(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getTennisCourts() {
+        return ResponseEntity.ok(courtService.getAllCourts());
+    }
 }
--- Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/service/TennisCourtService.java
+++ Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/service/TennisCourtService.java
@@ -1,4 +1,12 @@
 package com.levi9.internship.TennisScheduler.service;
 
+import com.levi9.internship.TennisScheduler.model.TennisCourt;
+
+import java.util.List;
+
 public interface TennisCourtService {
+
+    public TennisCourt getTennisCourt(Long id);
+
+    public List<TennisCourt> getAllCourts();
 }
