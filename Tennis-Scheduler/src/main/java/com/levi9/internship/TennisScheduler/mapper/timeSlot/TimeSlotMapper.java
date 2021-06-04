72c00b1e3f8ea755ae15bb04d394d3842b0d470e
Aleksandar Joksimovic <aleksandarjoksimovic996@gmail.com>
2021-06-04 14:28:13 +0200

    Changes in TennisCourtMapper

--- Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/mapper/timeSlot/TimeSlotMapper.java
+++ Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/mapper/timeSlot/TimeSlotMapper.java
@@ -1,8 +1,11 @@
 package com.levi9.internship.TennisScheduler.mapper.timeSlot;
 
+import com.levi9.internship.TennisScheduler.mapper.tennisCourt.TennisCourtMapper;
+import com.levi9.internship.TennisScheduler.model.TennisCourt;
 import com.levi9.internship.TennisScheduler.model.TimeSlot;
 import com.levi9.internship.TennisScheduler.modelDTO.timeSlot.TimeSlotDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TimeSlotMapper {
--- Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/modelDTO/timeSlot/TimeSlotDTO.java
+++ Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/modelDTO/timeSlot/TimeSlotDTO.java
@@ -16,6 +16,6 @@ public class TimeSlotDTO {
     private Long id;
     private LocalDateTime startDateAndTime;
     private LocalDateTime endDateAndTime;
-    private TennisCourtDTO tennisCourt;
-    //private ReservationDTO reservationId;
+    // private TennisCourtDTO tennisCourt;
+    // private ReservationDTO reservationId;
 }
