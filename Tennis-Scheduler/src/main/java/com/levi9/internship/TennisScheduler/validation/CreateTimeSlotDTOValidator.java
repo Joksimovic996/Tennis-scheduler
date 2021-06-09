7b17123b12d352989d6827d505e25e177f1e89e6
nikzejak <nik.zejak@gmail.com>
2021-06-09 13:26:14 +0200

    additional-time-slot-condition

--- Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/validation/CreateTimeSlotDTOValidator.java
+++ Tennis-Scheduler/src/main/java/com/levi9/internship/TennisScheduler/validation/CreateTimeSlotDTOValidator.java
@@ -4,6 +4,8 @@ import com.levi9.internship.TennisScheduler.modelDTO.timeSlot.CreateTimeSlotDTO;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.time.DayOfWeek;
+
 
 public class CreateTimeSlotDTOValidator implements ConstraintValidator<ValidCreateTimeSlotDTO, CreateTimeSlotDTO> {
     @Override
@@ -15,7 +17,21 @@ public class CreateTimeSlotDTOValidator implements ConstraintValidator<ValidCrea
     public boolean isValid(CreateTimeSlotDTO value, ConstraintValidatorContext context) {
 
         if (value.getStartDateAndTime().isBefore(value.getEndDateAndTime())){
-            return true;
+            System.out.println("PRVI IF: isBefore");
+            if ((value.getStartDateAndTime().getDayOfWeek() != DayOfWeek.SATURDAY) &&
+                    (value.getStartDateAndTime().getDayOfWeek() != DayOfWeek.SUNDAY)){
+                System.out.println("DRUGI IF: RADNI DAN");
+                if ((value.getStartDateAndTime().getHour() >= 18) && (value.getEndDateAndTime().getHour() <= 23)) {
+                    System.out.println("TRECI IF: DOBRO VREME");
+                    return true;
+                }
+            } else {
+                System.out.println("DRUGI IF: VIKEND");
+                if ((value.getStartDateAndTime().getHour() >= 17) && (value.getEndDateAndTime().getHour() <= 22)) {
+                    System.out.println("CETVRTI IF: DOBRO VREME");
+                    return true;
+                }
+            }
         }
 
         return false;
